489539ba03bfd55c795161d00f2838d357bf24c9
amaliebethune <71878499+amaliebethune@users.noreply.github.com>
2020-11-14 00:16:24 -0500

    edit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let bloom = document.querySelector('#bloom');
 
 bloom.onclick = () => {
-  if (bloom.getAttribute('src') === 'images/carnation_buds.jpg')
+  if (bloom.getAttribute('src') === 'js_project/images/carnation_buds.jpg')
     {
-      bloom.setAttribute('src', 'images/carnation.jpg');
+      bloom.setAttribute('src', 'js_project/images/carnation.jpg');
 } else {
-  bloom.setAttribute('src', 'images/carnation_buds.jpg');
+  bloom.setAttribute('src', 'js_project/images/carnation_buds.jpg');
 }
 };
 
@@ -14,10 +14,10 @@ let colors = document.querySelector('#colors');
 
 colors.onmouseenter = (e) => {
 	// change image
-   e.target.setAttribute('src', 'images/pink_carnation.jpg');
+   e.target.setAttribute('src', 'js_project/images/pink_carnation.jpg');
 };
 
 colors.onmouseleave = (e) => {
 // change image
-   e.target.setAttribute('src', 'images/moondust-carnation.jpg');
+   e.target.setAttribute('src', 'js_project/images/moondust-carnation.jpg');
 };
